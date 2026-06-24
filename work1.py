88f4e7dbae81a48470c32a3e43f98651900eeacc
ieatmyboIIs <fsafdf1.2v2@gmail.com>
2026-06-24 10:08:27 +0700

    dih
    dih

--- work1.py
+++ work1.py
@@ -1,6 +1,6 @@
-a = input("team1: ")
-b = input("team2: ")
-x = input("score1: ")
-y = input("score2: ")
-print(a + " vs " + b)
-print("final score " + x + " to " + y)
+team1 = "liverpool"
+team2 = "chelsea"
+score1 = 4
+score2 = 3
+print(team1 + " vs " + team2 )
+print(str(score1) + " to " + str(score2))
